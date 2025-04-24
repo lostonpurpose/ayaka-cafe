02aa0440e102b0208c1f5f40043ba9368fca001a
Will VanderWyden <vanderwyden@gmail.com>
2025-04-24 14:53:20 +0900

    cards indiv images done

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -4,14 +4,14 @@ interface CardProps {
   
    const Card = ({ imgSrc }: CardProps) => {
     return <div className="card-container">
-        <div className="card info flex justify-center">
+        <div className="card info flex justify-center border">
             <img className="w-50 h-60 aspect-square" src={imgSrc} alt="" />
         </div>
         <div className="info flex justify-center">
         <p>Hello</p>
         </div>
         <div className="info flex justify-center">
-        <button>Add to Cart</button>
+        <button className="button">Add to Cart</button>
         </div>
 
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,15 @@ import Card from "./components/card";
 export default function Home() {
   return (
     <>
-    <Card />
-    <Card />
+    <Card imgSrc="/pexels-chevanon-302904.jpg"/>
+    <Card imgSrc="/pexels-jayoke-851555.jpg"/>
+    <Card imgSrc="/pexels-julieaagaard-2351275.jpg"/>
+    <Card imgSrc="/pexels-mew-wy-1427872-2910874.jpg"/>
+    <Card imgSrc="/pexels-myfoodie-2638019.jpg"/>
+    <Card imgSrc="/pexels-chevanon-312418.jpg"/>
+    <Card imgSrc="/pexels-pixabay-414630.jpg"/>
+    <Card imgSrc="/pexels-valeriiamiller-3020919.jpg"/>
+    <Card imgSrc="/pexels-victorfreitas-685527.jpg"/>
     </>
  
   );
