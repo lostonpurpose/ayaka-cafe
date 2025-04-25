850f31d5aa24a080dd31e90128157598d46c919b
Will VanderWyden <vanderwyden@gmail.com>
2025-04-25 15:22:43 +0900

    fixed it myself, no thanks to cgpt

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -6,20 +6,18 @@ interface CardProps {
   const Card = ({ imgSrc, infoText }: CardProps) => {
     return (
       <div className="card-container">
-        <div className="card flex justify-center flex-col w-full">
+        <div className="card flex justify-center flex-col w-full relative">
           <img
             className="w-full h-60 object-cover mx-auto"
             src={imgSrc}
             alt=""
           />
-          <div className="info-button">
-            <div className="info flex justify-center">
-              <p>{infoText}</p>
-            </div>
-            <div className="info flex justify-center">
-              <button className="button w-full border rounded border-gray-500 bg-blue-300">Add to Cart</button>
-            </div>
+          <div className="info absolute top-0 w-full bg-black/50">
+              <p className="text-sm text-blue-300">{infoText}</p>
           </div>
+        </div>
+        <div className="flex justify-center pt-1">
+          <button className="button w-full border rounded border-gray-500 bg-blue-300">Add to Cart</button>
         </div>
       </div>
     );
