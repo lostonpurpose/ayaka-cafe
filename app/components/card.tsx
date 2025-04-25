d9bccc71530b97143a73dd871453f13bb99db3f9
Will VanderWyden <vanderwyden@gmail.com>
2025-04-25 14:37:38 +0900

    slow going. format correct

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -1,20 +1,29 @@
 interface CardProps {
     imgSrc: string;
+    infoText: string;
   }
   
-   const Card = ({ imgSrc }: CardProps) => {
-    return <div className="card-container">
-        <div className="card info flex justify-center border">
-            <img className="w-50 h-60 aspect-square" src={imgSrc} alt="" />
+  const Card = ({ imgSrc, infoText }: CardProps) => {
+    return (
+      <div className="card-container">
+        <div className="card flex justify-center flex-col w-full">
+          <img
+            className="w-full h-60 object-cover mx-auto"
+            src={imgSrc}
+            alt=""
+          />
+          <div className="info-button">
+            <div className="info flex justify-center">
+              <p>{infoText}</p>
+            </div>
+            <div className="info flex justify-center">
+              <button className="button w-full border rounded border-gray-500 bg-blue-300">Add to Cart</button>
+            </div>
+          </div>
         </div>
-        <div className="info flex justify-center">
-        <p>Hello</p>
-        </div>
-        <div className="info flex justify-center">
-        <button className="button">Add to Cart</button>
-        </div>
-
-        </div>
-}
-
-export default Card;
+      </div>
+    );
+  };
+  
+  export default Card;
+  
--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,22 @@ import Card from "./components/card";
 export default function Home() {
   return (
     <>
-    <Card imgSrc="/pexels-chevanon-302904.jpg"/>
-    <Card imgSrc="/pexels-jayoke-851555.jpg"/>
-    <Card imgSrc="/pexels-julieaagaard-2351275.jpg"/>
-    <Card imgSrc="/pexels-mew-wy-1427872-2910874.jpg"/>
-    <Card imgSrc="/pexels-myfoodie-2638019.jpg"/>
-    <Card imgSrc="/pexels-chevanon-312418.jpg"/>
-    <Card imgSrc="/pexels-pixabay-414630.jpg"/>
-    <Card imgSrc="/pexels-valeriiamiller-3020919.jpg"/>
-    <Card imgSrc="/pexels-victorfreitas-685527.jpg"/>
+      <div className="flex w-full">
+        <div className="grid grid-cols-3 gap-4 flex-1" infoText="poop">
+          <Card imgSrc="/pexels-chevanon-302904.jpg" />
+          <Card imgSrc="/pexels-jayoke-851555.jpg" />
+          <Card imgSrc="/pexels-julieaagaard-2351275.jpg" />
+          <Card imgSrc="/pexels-mew-wy-1427872-2910874.jpg" />
+          <Card imgSrc="/pexels-myfoodie-2638019.jpg" />
+          <Card imgSrc="/pexels-chevanon-312418.jpg" />
+          <Card imgSrc="/pexels-pixabay-414630.jpg" />
+          <Card imgSrc="/pexels-valeriiamiller-3020919.jpg" />
+          <Card imgSrc="/pexels-victorfreitas-685527.jpg" />
+        </div>
+        <div className="w-[31%] bg-gray-200">
+          <h1>hello</h1>
+        </div>
+      </div>
     </>
- 
   );
 }
