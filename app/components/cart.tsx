525a0a02d83a04db070c9e5e4494c5ced8da93e5
Will VanderWyden <vanderwyden@gmail.com>
2025-05-17 14:03:06 +0900

    much improved cart

--- app/components/cart.tsx
+++ app/components/cart.tsx
@@ -11,24 +11,29 @@ const Cart = ({ cartItems }: CartProps) => {
       <div className="flex justify-center bg-white">
         <img className="object-cover w-30 h-15" src="/logo-c.jpg" alt="Cart Logo" />
       </div>
-      <div className="your-cart">
-        <h1>You Cart</h1>
+      <div className="your-cart bg-blue-300 p-4 mb-5">
+        <div className="mb-3">
+          <h1>Your Cart</h1>
+        </div>
+      
+        <div className="items">
+          <ul className="bg-white">
+            {cartItems.map((item, index) => (
+              <li key={index} className="item-container flex justify-between border border-blue-300 p-2">
+                <div className="item-name">
+                  <h1>{item.infoText}</h1>
+                </div>
+                <div className="item-price">
+                  <h1>${item.price}</h1>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+
       </div>
-      <div className="items">
-        <ul className="bg-blue-300">
-          {cartItems.map((item, index) => (
-            <li key={index} className="item-container flex justify-between border border-white p-2">
-              <div className="item-name">
-                <h1>{item.infoText}</h1>
-              </div>
-              <div className="item-price">
-                <h1>${item.price}</h1>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="subtotal flex justify-between">
+      <hr />
+      <div className="subtotal flex justify-between mt-4">
         <h2>Subtotal: ¥</h2>
         <h2>
             {cartItems.reduce((total, item) => total + item.price, 0).toFixed(2)}
